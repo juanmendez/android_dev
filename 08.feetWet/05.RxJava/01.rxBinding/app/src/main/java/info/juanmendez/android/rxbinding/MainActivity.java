61810ce72d290b7d8f12a0446c581faf8f0494b1
juanmendez <contact@juanmendez.info>
2015-12-04 21:46:48 -0600

    added button listeners

--- 08.feetWet/05.RxJava/01.rxBinding/app/src/main/java/info/juanmendez/android/rxbinding/MainActivity.java
+++ 08.feetWet/05.RxJava/01.rxBinding/app/src/main/java/info/juanmendez/android/rxbinding/MainActivity.java
@@ -1,9 +1,11 @@
 package info.juanmendez.android.rxbinding;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.widget.RxTextView;
 
 import org.androidannotations.annotations.AfterViews;
@@ -13,16 +15,31 @@ import org.androidannotations.annotations.ViewById;
 @EActivity (R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @ViewById(R.id.textView)
+    @ViewById
     TextView textView;
 
-    @ViewById(R.id.editText)
+    @ViewById
     EditText editText;
+
+    @ViewById
+    Button clearButton;
+
+    @ViewById
+    Button reverseButton;
 
     @AfterViews
     void afterViews(){
+
         RxTextView.textChanges( editText ).subscribe(charSequence -> {
             textView.setText( charSequence );
+        });
+
+        RxView.clicks(clearButton).subscribe(aVoid -> {
+            editText.setText("");
+        });
+
+        RxView.clicks(reverseButton).subscribe(aVoid -> {
+            editText.setText( new StringBuilder(textView.getText()).reverse());
         });
     }
 }
