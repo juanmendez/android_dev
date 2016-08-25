e94fb3ee849f8dcd288222a0031570c22b3a725c
Juan Mendez <contact@juanmendez.info>
2016-08-25 14:49:02 -0500

    updated 01.ream+mcc

--- 09.database_connection/07.realm/01.realm_mcc/app/src/main/java/info/juanmendez/realminit/MainActivity.java
+++ 09.database_connection/07.realm/01.realm_mcc/app/src/main/java/info/juanmendez/realminit/MainActivity.java
@@ -3,16 +3,13 @@ package info.juanmendez.realminit;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 import info.juanmendez.realminit.models.Song;
 import io.realm.Realm;
-import io.realm.RealmResults;
+import io.realm.RealmChangeListener;
 
 /**
  * @author Juan Mendez
@@ -23,16 +20,31 @@ public class MainActivity extends AppCompatActivity {
     private Realm realm;
     private static final String TAG = "MainActivity";
 
+    RealmChangeListener changeListener = new RealmChangeListener() {
+        @Override
+        public void onChange(Object element) {
+            showLastSongCount();
+        }
+    };
+
+    private void showLastSongCount(){
+        long count = realm.where(Song.class).count();
+        setTitle( "Number of songs " + count );
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         realm = Realm.getDefaultInstance();
+        realm.addChangeListener( changeListener );
+        showLastSongCount(); //start showing number of songs..
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         realm.close();
+        realm.removeAllChangeListeners();
     }
 
     @Click(R.id.addSongButton)
@@ -49,35 +61,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This activity could have used an interface, and event a bus communication between
+     * This activity could have used an interface, or an event a bus communication between
      * its fragment, but for simplicity and demonstration it is a straight method call.
-     * @param song
      */
-    public void onSongSubmitted( Song song ){
+    public void kickMeOut(){
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment songFragment = fm.findFragmentByTag("songFormFragment");
-        fm.beginTransaction().remove(  songFragment ).commit();
 
-        /**
-         * Asynchronous Transaction
-         */
-
-        realm.executeTransactionAsync( thisRealm->{
-            /**
-             * you can create an instance of an object first and add it later using realm.copyToRealm()
-             */
-
-            int nextID = (thisRealm.where(Song.class).max("id")).intValue() + 1;
-            song.setId( nextID );
-            Song s = thisRealm.copyToRealm( song );
-        },
-        ()->{
-            Log.i( TAG, "song has been created");
-        },
-        error->{
-            Log.e( TAG, "an error while saving " + error.getMessage() );
-        });
-
+        if( songFragment != null )
+            fm.beginTransaction().remove(  songFragment ).commit();
     }
 }
--- 09.database_connection/07.realm/01.realm_mcc/app/src/main/java/info/juanmendez/realminit/SongFormFragment.java
+++ 09.database_connection/07.realm/01.realm_mcc/app/src/main/java/info/juanmendez/realminit/SongFormFragment.java
@@ -4,16 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
 import info.juanmendez.realminit.models.Song;
+import io.realm.Realm;
 
-/**
- * Created by musta on 8/22/2016.
- */
 @EFragment(R.layout.song_form)
 public class SongFormFragment extends Fragment {
     private MainActivity mainActivity;
@@ -26,10 +25,13 @@ public class SongFormFragment extends Fragment {
     @ViewById
     EditText yearInput;
 
+    private Realm realm;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mainActivity = (MainActivity) getActivity();
+        realm = Realm.getDefaultInstance();
     }
 
     public static SongFormFragment makeForm(){
@@ -44,6 +46,18 @@ public class SongFormFragment extends Fragment {
         song.setVideo_url( urlInput.getText().toString() );
         song.setYear( Integer.parseInt( yearInput.getText().toString()) );
 
-        mainActivity.onSongSubmitted( song );
+        realm.executeTransactionAsync( thisRealm->{
+            int nextID = (thisRealm.where(Song.class).max("id")).intValue() + 1;
+
+            /*you can create an instance of an object first and add it later using realm.copyToRealm()*/
+            song.setId( nextID );
+            Song s = thisRealm.copyToRealm( song );
+        },
+        ()->{
+            mainActivity.kickMeOut();
+        },
+        error->{
+            Toast.makeText( mainActivity, "an error while saving " + error.getMessage(), Toast.LENGTH_LONG ).show();
+        });
     }
 }
