3d160cd64a776c5b5931da17595193e00a139769
Juan Mendez <juan.mendez@grainger.com>
2018-11-27 16:21:29 -0600

    object box plugin + icon

--- 09.database_connection/08.object_box/app/src/main/java/info/juanmendez/objectbox/MainActivity.kt
+++ 09.database_connection/08.object_box/app/src/main/java/info/juanmendez/objectbox/MainActivity.kt
@@ -1,7 +1,7 @@
 package info.juanmendez.objectbox
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
